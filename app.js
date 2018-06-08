13437d656e0192dc30d11ce911c2efb674142107
SiaKovalina <siakovalina@gmail.com>
2018-06-08 19:17:40 +0300

    logic added

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
 //Listen for submit 
-document.querySelector('#loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
 
 function calculateResults(e) {
-    const $amount = document.querySelector('#amout');
-    const $interest = document.querySelector('#interest');
-    const $years = document.querySelector('#years');
-    const $monthlyPayment = document.querySelector('#monthly-payment');
 
     e.preventDefault();
+    const amount = document.querySelector('#amount');
+    const interest = document.querySelector('#interest');
+    const years = document.querySelector('#years');
+    const monthlyPayment = document.querySelector('#monthly-payment');
+    const totalPayment = document.querySelector('#total-payment');
+    const totalInterest = document.querySelector('#total-interest');
+
+    const principle = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+
+    //Compute the monthly payments
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principle * x * calculatedInterest) / (x - 1);
+    
+    if (isFinite(monthly)) {
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+        totalInterest.value = ((monthly * calculatedPayments) - principle).toFixed(2);
+    } else {
+        showError('Please, check your numbers');
+    }
+   
+}
+
+function showError(error) {
+    const errorDiv = document.createElement('div');
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+
+    errorDiv.className = 'alert alert-danger';
+    errorDiv.appendChild(document.createTextNode(error));
+
+    card.insertBefore(errorDiv, heading);
+
+    setTimeout(clearError, 3000);
+}
+
+function clearError() {
+    document.querySelector('.alert').remove();
 }
